961edd7e98108e6deac5667908e5a9fa88e11a6d
Sebin Song <songxevin@gmail.com>
2022-05-13 00:46:20 +1200

    add fonts

--- src/js/components/header/Header.js
+++ src/js/components/header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 import './Header.scss'
+import logoPath from '@images/logo.svg'
 
 const Header = () => {
   return (
-    <header className="l-toolbar">Header</header>
+    <header className="l-toolbar app-header">
+      <div className="app-header__content">
+        <img className="app-header__logo"
+          src={logoPath}
+          alt="A tiny shop logo" />
+
+        <h1 className="app-header__app-title">A tiny shop</h1>
+
+      </div>
+    </header>
   )
 }
 
