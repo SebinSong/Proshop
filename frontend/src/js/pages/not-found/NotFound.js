9d48675c447fcb60c8a8e04b15667e46285d1e4c
SebinSong <songxevin@gmail.com>
2023-07-23 20:16:33 +1200

    better-design the not-found page

--- frontend/src/js/pages/not-found/NotFound.js
+++ frontend/src/js/pages/not-found/NotFound.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './NotFound.scss'
 
 export default function NotFound () {
+  const navigate = useNavigate()
+
   return (
-    <h3>404 Not found</h3>
+    <main className='page-404'>
+      <h1 className='is-title-2 err-title'>Oops! Something gone wrong.</h1>
+      <p className='err-desc'>Our server might have experienced some issue.</p>
+      <a href='/login'>Retry</a>
+    </main>
   )
 }
